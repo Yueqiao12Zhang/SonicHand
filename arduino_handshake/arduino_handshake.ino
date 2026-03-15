1b3928f073bb398b0bd66de56b0b13f78aaaacae
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2026-03-15 17:51:34 -0400

    arduino create

---
+++ arduino_handshake/arduino_handshake.ino
@@ -0,0 +1,26 @@
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  if (distance > 10 && distance < 100) {
+    Serial.println(distance);
+  }
+  
+  delay(30);
+}
